2cb5fba4018784147c3f68c673d9a09b8d58287a
MohamedJellali <71153944+MohamedJellali@users.noreply.github.com>
2020-12-16 00:59:00 +0100

    Update App.js

--- checkpointprops/src/App.js
+++ checkpointprops/src/App.js
@@ -1,6 +1,6 @@
-
 import './App.css';
 import ProfileComponent from './profile/ProfileComponent';
+import nameOfUser from './profile/ProfileComponent'
 
 function App() {
 
@@ -8,11 +8,15 @@ function App() {
     <div className="App">
 
       <ProfileComponent 
-
       name="Vincent Van-Gogh" 
-      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ekhDtjfbUjaE4HAGsizdqtFEbBnCPZGcmg&usqp=CAU"
       bio="Dutch painter, generally considered the greatest, and one of the greatest of the Post-Impressionists" 
-      profession="painter" />
+      profession="painter" 
+      nameOfUser={nameOfUser}
+      > 
+      <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ekhDtjfbUjaE4HAGsizdqtFEbBnCPZGcmg&usqp=CAU" alt="Van-gogh" height="420" style={{marginBottom : 20, marginTop : 10, borderRadius:17}} />
+      </div>
+      </ProfileComponent>
 
     </div>
   );
