9b880d59c78c2b388912032dde7f9ec8e80e86e1
MohamedJellali <71153944+MohamedJellali@users.noreply.github.com>
2020-12-23 23:13:40 +0100

    Update App.js

--- checkpointprops/src/App.js
+++ checkpointprops/src/App.js
@@ -1,25 +1,40 @@
-import './App.css';
-import ProfileComponent from './profile/ProfileComponent';
-import handelName from './profile/ProfileComponent'
+import React from 'react'
+import PropTypes from "prop-types";
+import "../App.css";
 
-function App() {
 
-  return (
-    <div className="App">
+export default function ProfileComponent(props) {
 
-      <ProfileComponent 
-      name="Vincent Van-Gogh" 
-      bio="Dutch painter, generally considered the greatest, and one of the greatest of the Post-Impressionists" 
-      profession="painter" 
-      handelName={handelName}
-      > 
-      <div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ekhDtjfbUjaE4HAGsizdqtFEbBnCPZGcmg&usqp=CAU" alt="Van-gogh" height="420" style={{marginBottom : 20, marginTop : 10, borderRadius:17}} />
-      </div>
-      </ProfileComponent>
+   
 
-    </div>
-  );
+    return (
+        <div>
+            {console.log(props.children)}
+            <h1  style={{color: "red"}}>I am {props.name}</h1>
+            <div>{props.children}</div>
+            <p style={{backgroundColor:"#0BEFEC"}} >I am a {props.bio}</p>
+            <p>I am a {props.profession}</p>
+            <button style={{backgroundColor:"#0BEFEC"}} onClick={()=> props.handelName(props.name)}>  Display my name </button>
+   
+        </div>
+    )
 }
 
-export default App;
+ProfileComponent.defaultProps = {
+    name: "Give me a name :,(",
+    Bio:"Write my life :,(",
+    Profession:"Who is me :,(",
+    };
+
+ProfileComponent.propTypes = {
+    name: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    profession: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    handelName: PropTypes.func
+  };
+
+
+
+
+
